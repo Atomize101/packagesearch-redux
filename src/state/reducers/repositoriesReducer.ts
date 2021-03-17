410da48b474e3f8db0ceaa550f2cba036d8198a6
Chris Patrick <atomize101@gmail.com>
2021-03-17 12:35:38 -0400

    Adding in action interfaces and typeguard

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -4,16 +4,35 @@ interface RepositoriesState {
 	data: string[];
 }
 
-interface Action {
-	type: string;
-	payload?: any;
+interface SearchRepositoriesAction {
+	type: 'search_repositories';
 }
 
-const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
+interface SearchRepositoriesSuccessAction {
+	type: 'search_repositories_success';
+	payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+	type: 'search_repositories_error';
+	payload: string;
+}
+
+const reducer = (
+	state: RepositoriesState,
+	action: SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction
+): RepositoriesState => {
+	if (action.type === 'search_repositories_success') {
+		// 100% certain that 'action satisfies
+		// the SearchRepositoriesSuccessAction interface
+	}
+
 	switch (action.type) {
 		case 'search_repositories':
 			return { loading: true, error: null, data: [] };
 		case 'search_repositories_success':
+			// 100% certain that 'action' is SearchRepositoriesSuccessAction
+			// Just like the if statement
 			return { loading: false, error: null, data: action.payload };
 		case 'search_repositories_error':
 			return { loading: false, error: action.payload, data: [] };
